8d904e7b81e3be6b2fc595cc25470044dfeeac62
Seunghun Lee <waydi1@gmail.com>
2017-01-30 19:34:36 -0800

    Apply new model
    This model is based on NVIDIA's "End to End Learning for Self-Driving Cars".
    https://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf

--- drive.py
+++ drive.py
@@ -15,6 +15,8 @@ from io import BytesIO
 from keras.models import model_from_json
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
 
+import cv2
+
 # Fix error with Keras and TensorFlow
 import tensorflow as tf
 tf.python.control_flow_ops = tf
@@ -37,6 +39,7 @@ def telemetry(sid, data):
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
     image_array = np.asarray(image)
+    image_array = cv2.resize(image_array, (200, 66))
     transformed_image_array = image_array[None, :, :, :]
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
--- model.py
+++ model.py
@@ -3,8 +3,11 @@ import json
 
 import cv2
 import numpy as np
+
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten
+from keras.layers.convolutional import Convolution2D
+from keras.layers.normalization import BatchNormalization
 
 
 DRIVING_LOG_FILE = 'driving_log.csv'
@@ -17,10 +20,10 @@ def load_train_data():
     steering_angles = []
 
     with open(DRIVING_LOG_FILE, 'rt') as f:
-        log_reader = csv.reader(f, delimiter=',')
+        log_reader = csv.reader(f, skipinitialspace=True)
         for row in log_reader:
             images.append(cv2.imread(row[0]))
-            steering_angles.append(row[3])
+            steering_angles.append(float(row[3]))
 
     images = np.array(images)
     steering_angles = np.array(steering_angles)
@@ -30,9 +33,38 @@ def load_train_data():
 
 def get_model():
     model = Sequential()
-    model.add(Flatten(input_shape=(160, 320, 3)))
+
+    model.add(BatchNormalization(input_shape=(66, 200, 3)))
+
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2)))
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2)))
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2)))
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(64, 3, 3))
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(64, 3, 3))
+    model.add(Activation('relu'))
+
+    model.add(Flatten())
+
+    model.add(Dense(1164))
+    model.add(Activation('relu'))
+
     model.add(Dense(100))
     model.add(Activation('relu'))
+
+    model.add(Dense(50))
+    model.add(Activation('relu'))
+
+    model.add(Dense(10))
+    model.add(Activation('relu'))
+
     model.add(Dense(1))
 
     return model
@@ -42,8 +74,8 @@ def train_model(model, X_train, y_train):
     # Hyperparameters
     EPOCHS = 5
     BATCH_SIZE = 128
-    model.compile('adam', 'mse', ['accuracy'])
-    history = model.fit(X_train, y_train, nb_epoch=5, batch_size=128,
+    model.compile('adam', 'mse')
+    history = model.fit(X_train, y_train, nb_epoch=EPOCHS, batch_size=BATCH_SIZE,
                         validation_split=0.2, shuffle=True)
 
     # Store model in file
@@ -54,7 +86,13 @@ def train_model(model, X_train, y_train):
     model.save_weights(WEIGHTS_FILE)
 
 
+def preprocess(images):
+    images = np.array([cv2.resize(img, (200, 66)) for img in images])
+    return images
+
 if __name__ == '__main__':
     X_train, y_train = load_train_data()
+    X_train = preprocess(X_train)
     model = get_model()
+    model.summary()
     train_model(model, X_train, y_train)
