4c6ca9c6a91623f57221716373a832a0c8b425f9
Seunghun Lee <waydi1@gmail.com>
2017-02-06 00:06:44 -0800

    Change activation function from ReLU to ELU

--- model.py
+++ model.py
@@ -11,33 +11,33 @@ def get_model():
     model.add(BatchNormalization(input_shape=(66, 200, 3)))
 
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2)))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
     model.add(Convolution2D(36, 5, 5, subsample=(2, 2)))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
     model.add(Convolution2D(48, 5, 5, subsample=(2, 2)))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
     model.add(Convolution2D(64, 3, 3))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
     model.add(Convolution2D(64, 3, 3))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
     model.add(Flatten())
 
     model.add(Dense(1164))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
     model.add(Dense(100))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
     model.add(Dense(50))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
     model.add(Dense(10))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
     model.add(Dense(1))
 
